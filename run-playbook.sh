9067549e7f8f57b7871e31df4abff6ab02f64248
SamBritten <sbritten315@gmail.com>
2019-11-29 11:09:16 +0000

    added TLS certificates

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# NGINX
-ansible-playbook \
-    -v -i "$(terraform output nginx_fqdn), \
-    -u jenkins \
-    --ssh-common-args="-o StrictHostKeyChecking=no" playbooks/nginx.yml
-
 # JENKINS
 ansible-playbook \
-    -v -i "$(terraform output jenkins_fqdn), \
+    -i "$(terraform output jenkins_fqdn)," \
     -u jenkins \
     --ssh-common-args="-o StrictHostKeyChecking=no" playbooks/jenkins.yml
+
+# NGINX
+ansible-playbook \
+    -i "$(terraform output nginx_fqdn)," \
+    -u jenkins \
+    --ssh-common-args="-o StrictHostKeyChecking=no" playbooks/nginx.yml
